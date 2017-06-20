2d12754fb59e07941ae4136cd3a5cd35578eca31
DanRamage <ChiefDan@gmail.com>
2017-06-20 08:15:28 -0400

    INit checkin.

---
+++ scripts/saluda_river_engine.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
+cd /home/xeniaprod/scripts/Saluda-River-Project/scripts;
+
+python /home/xeniaprod/scripts/Saluda-River-Project/scripts/sc_rivers_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Saluda-River-Project/config/sc_rivers_engine.ini
