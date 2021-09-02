27ba8a1d4804e0dce32d44e5e644fcdc7849f63e
DANRAMAGE <dan@inlet.geol.sc.edu>
2021-09-02 10:51:04 -0400

    Update the virualenv activate.

--- scripts/saluda_river_engine.sh
+++ scripts/saluda_river_engine.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 
-source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
+#source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
+source /usr/local/virtualenv/pyenv-3.8.5/bin/activate
 cd /home/xeniaprod/scripts/Saluda-River-Project/scripts;
 
 python /home/xeniaprod/scripts/Saluda-River-Project/scripts/sc_rivers_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Saluda-River-Project/config/sc_rivers_engine.ini
